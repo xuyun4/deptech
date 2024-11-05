80e85c9ec46b8b9ca3db86c150a3ded286a1019c
liuyongqin <2841150302@qq.com>
2024-11-05 11:20:33 +0800

    登录鉴权完成

--- src/main/java/com/example/daptech/mapper/PhoneCnMapper.java
+++ src/main/java/com/example/daptech/mapper/PhoneCnMapper.java
@@ -1,16 +1,16 @@
 package com.example.daptech.mapper;
 
 import com.example.daptech.entity.PhoneCn;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface PhoneCnMapper {
 
     @Insert("INSERT INTO phone_cn(phone, type, number, status, create_time, update_time, location) VALUES (#{phone}, #{type}, #{number}, #{status}, #{createTime}, #{updateTime}, #{location})")
     void insertPhoneCn(String phone, String type,Integer number,Integer status,long createTime,long updateTime,String location);
+
+    @Delete("DELETE FROM phone_cn WHERE phone = #{phone}")
+    void deletePhoneCn(String phone);
     @Update("UPDATE phone_cn SET type = #{type}, number = #{number} AND update_time = #{updateTime} WHERE phone = #{phone}")
     void updatePhoneType(String phone, String type,Integer number,long updateTime);
     @Update("UPDATE phone_cn SET information = #{info} WHERE phone = #{phone}")
