ccc099398b65299af91ba3f3ee893b55062de143
xuyun <biankangning@126.com>
2024-11-27 20:17:01 +0800

    result更改

--- src/main/java/com/example/daptech/response/Result.java
+++ src/main/java/com/example/daptech/response/Result.java
@@ -4,28 +4,50 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 
 //这是返回前端的统一返回类
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Result {
-    private Integer code;//响应码，1 代表成功; 0 代表失败
-    private String msg;  //响应信息 描述字符串
-    private Object data; //返回的数据
+public class Result<T> implements Serializable {
 
-    //增删改 成功响应
-    public static Result success() {
-        return new Result(1, "success", null);
+    public static final String SUCCESS_MSG="success";
+
+    private Integer code;//响应码，1 代表成功; 0 代表失败 (2 代表用户名密码等格式错误)
+    private String msg;  //响应信息 描述字符串
+    private T data; //返回的数据
+
+    //成功响应 不返回数据
+    public static <T> Result<T> success() {
+        Result<T> result = new Result<T>();
+        result.code = 1;
+        result.msg = SUCCESS_MSG;
+        return result;
     }
 
-    //查询 成功响应
-    public static Result success(Object data) {
-        return new Result(1, "success", data);
+    //成功响应 返回数据
+    public static <T> Result<T> success(T data) {
+        Result<T> result = new Result<T>();
+        result.code = 1;
+        result.msg = SUCCESS_MSG;
+        result.data = data;
+        return result;
     }
 
     //失败响应
-    public static Result error(String msg) {
-        return new Result(0, msg, null);
+    public static <T> Result<T> error(String msg) {
+        Result<T> result = new Result<T>();
+        result.code = 0;
+        result.msg = msg;
+        return result;
+    }
+
+    //格式错误响应
+    public static <T> Result<T> formatError(String msg) {
+        Result<T> result = new Result<T>();
+        result.code = 2;
+        result.msg = msg;
+        return result;
     }
 }
