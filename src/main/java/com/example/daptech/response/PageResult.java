41e5baa4abfd97b5fe3b70b52b0d2e4f41a44acc
xuyun <biankangning@126.com>
2024-12-17 13:40:02 +0800

    pageresult

--- src/main/java/com/example/daptech/response/PageResult.java
+++ src/main/java/com/example/daptech/response/PageResult.java
@@ -10,7 +10,10 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PageResult implements Serializable {
-    private long total;
-    private List records;
+public class PageResult<T> implements Serializable {
+    private long total;           // 总记录数
+    private int pageNum;          // 当前页码
+    private int pageSize;         // 每页显示的条目数
+    private int totalPages;       // 总页数
+    private List<T> Data;         // 数据列表
 }
