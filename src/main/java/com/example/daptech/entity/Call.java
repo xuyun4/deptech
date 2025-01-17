27234daabed5115eaa49f8854b48405d93374252
虚允 <biankangning@126.com>
2025-01-17 13:33:41 +0800

    Update Call.java

--- src/main/java/com/example/daptech/entity/Call.java
+++ src/main/java/com/example/daptech/entity/Call.java
@@ -3,6 +3,7 @@ package com.example.daptech.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class Call {
     private Integer id;
     private String name;
     private String phoneNumber;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime time;
     private String type;
     private String location;
