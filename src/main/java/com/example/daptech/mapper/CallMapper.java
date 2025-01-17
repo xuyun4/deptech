dcf6e7c33324c3b9a87717dce28ddc5fb34c286d
虚允 <biankangning@126.com>
2025-01-17 13:35:35 +0800

    Update CallMapper.java

--- src/main/java/com/example/daptech/mapper/CallMapper.java
+++ src/main/java/com/example/daptech/mapper/CallMapper.java
@@ -4,10 +4,16 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.daptech.entity.Call;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface CallMapper extends BaseMapper<Call> {
     Integer getNumberByMap(Map map);
 
+    void deleteCall(Long userId);
+
+    void addCall(List<Call> callList);
+
+    List<Call> selectCall(Long userId);
 }
